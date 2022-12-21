7e01b34d8cc185037b9449941a73ccdcc9101c57
Martin Frick <martin.frick@sap.com>
2022-12-21 08:12:16 +0100

    Node 16 + Refactor UI modules & RepoMock

--- app/uimodule/webapp/admin/projects/webapp/Component.js
+++
@@ -1,9 +0,0 @@
-sap.ui.define(['sap/fe/core/AppComponent'], function(AppComponent) {
-    'use strict';
-
-    return AppComponent.extend("susaas.admin.projects.mainApp.Component", {
-        metadata: {
-            manifest: "json"
-        }
-    });
-});
--- app/uimodule/webapp/public/assessments/webapp/Component.js
+++
@@ -1,9 +0,0 @@
-sap.ui.define(['sap/fe/core/AppComponent', "sap/ui/core/Component"], function(AppComponent, Component) {
-    'use strict';
-
-    return AppComponent.extend("susaas.assessments.mainApp.Component", {
-        metadata: {
-            manifest: "json"
-        }
-    });
-});
