95df5a2ca04a8796abc353684eec5414e9f49d98
Alper <a.dedeoglu@sap.com>
2023-07-22 22:45:46 +0200

    This commit has following fixes - CDS v7 upgrade - Replacement of OData V2 converter with community plugin - Removal of obsolete code from server.js

--- srv/server.js
+++ srv/server.js
@@ -1,8 +1,6 @@
 const cds = require('@sap/cds');
 
 const cdsSwagger = require('cds-swagger-ui-express');
-
-const odatav2adapterproxy = require('@sap/cds-odata-v2-adapter-proxy');
 const xsenv = require('@sap/xsenv');
 xsenv.loadEnv();
 
@@ -12,10 +10,6 @@ cds.on('bootstrap', async (app) => {
         "basePath": "/swagger",
         "diagram": "true"
     }));
-
-    app.use(odatav2adapterproxy());
-    // Here
-    
 });
 cds.on('served', async () => {
     const { 'cds.xt.SaasProvisioningService': provisioning } = cds.services
